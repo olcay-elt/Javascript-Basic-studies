faba019266b9063bedcaae1ddaaf4b216c0cc287
olcay-elt <olcay.elt@gmail.com>
2023-01-15 23:00:02 +0100

    Add new file

--- class-notes/DE-04-Functions/5-recursive.js
+++ class-notes/DE-04-Functions/5-recursive.js
@@ -48,3 +48,42 @@ const fibo = (n) => {
 console.log(fibo(8));
 
 
+//!Soru-1.Ücgenin Alani*****
+
+const area = (t, h) => (t * h) / 2
+
+console.log(area(6, 5).toFixed(2))
+
+
+
+
+//!Soru-2:  a) KareAl ******
+
+const sayi1 = +prompt("Bir sayi giriniz")
+
+const kare = (sayi) => sayi * sayi
+
+console.log(kare(sayi1))
+
+
+
+
+//!Soru-2:  b) KüpAl ******
+
+const sayi2 = +prompt("Bir sayi giriniz")
+
+const küp = (sayi2) => sayi2 * sayi2 * sayi2
+
+console.log(küp(sayi2))
+
+
+
+
+
+//!Soru-2:  c) ÜsAl ******
+
+const sayi3 = +prompt("Bir sayi giriniz")
+
+const usAl = (sayi3) => Math.pow(sayi3)
+
+console.log(usAl(sayi3))
