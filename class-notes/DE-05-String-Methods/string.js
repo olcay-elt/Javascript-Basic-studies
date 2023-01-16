b7fdbe5e414e256429a162c0ed6b7a9741d2f83e
olcay-elt <olcay.elt@gmail.com>
2023-01-16 12:40:08 +0100

    Add new files

--- class-notes/DE-05-String-Methods/string.js
+++ class-notes/DE-05-String-Methods/string.js
@@ -1,13 +1,18 @@
 // ?=========================================================
 // ?                 STRING METOTLARI
 // ?=========================================================
-console.log("****STRİNG METHODS******");
+console.log("****STRING METHODS******");
 
-//!Klasik yöntem ile tanimlanan String primitive dir
-(ilkel)
+//! 1- Klasik yöntem ile tanımlanan String primitive dir (ilkel)
+const str1 = "Clarusway"
+const str2 = "hello"
+const str3 = "all the world"
+console.log(str1 + str2, typeof (str1 + str2));
 
-const str4 = new String("yeni bir string")
-console.log(str4);
+//! 2- String constructor ile tanımlanan String, non-primitive dir (ilkel olmayan)
+
+const str4 = new String("yeni bir String")
+console.log(str4, typeof str4);
 
 //! Normalde primitive veri tiplerinin property veya metodu olmaz.
 //! Ancak Javascript  otomatik olarak primitive String'leri String
@@ -16,81 +21,83 @@ console.log(str4);
 //! mümkün olur. Ayrıca 2 tür arasında çevrim yapmakta mümkündür.
 //? Ek Açıklama için : https://javascript.info/primitives-methods
 
-
 // *=========================================================
 // *               concat() immutable=değiştirmez
 // *=========================================================
 
 let s1 = "hello"
 const s2 = "world"
+
 console.log(s1.concat(s2))
-const s3 = s1.concat(s2)
-console.log(s3)
+console.log(s1);
+const s3 = s1.concat(s2);
+console.log(s3);
 
+console.log(s3.concat(" ", "clarusway", " ", s1));
 
-
+//!orjinal veri değişmez, assigning ,ile kalıcı değişiklik yapabiliriz
 
 // *=========================================================
-// *             toUpperCase(), toLowerCase(), immutable
+// *       toUpperCase(), toLowerCase() immutable
 // *=========================================================
 
-const esitMi = (str1, str2) => str1 === str2.toLocaleUpperCase("tr") ? "esittir" : "esit degildir";
+const myName = "Ashley Miller "
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
 
-console.log(esitMi("ISMET", "ismet"));
+console.log(myName.toLocaleUpperCase("tr"));
 
 //! toLocaleUpperCase("tr")= Türkçe büyüt demek  boş bırakınca  eng (ingilizce) karakterlere uygun büyütebiliriz
 
 
+const esitMi = (str1, str2) => str1 === str2.toLocaleUpperCase("tr") ? "esittir" : "esit değildir";
 
-
-
-// *=========================================================
-// *            charAt() immutable
-// *=========================================================
-
-const s4 = "primitive \n veri tiplerinin \n property veya metodu olmaz"
-
-console.log(s4.charAt(5));
-console.log(s4.charAt(8));
-console.log(s4.charAt(s4.length - 1)); //! sonuncu karakter
-
-
-
-
+console.log(esitMi("İSMET", "ismet"));
 
 // *=========================================================
-// *           includes() -case sensitive (kücük-büyük harf duyarli )
+// *       charAt() immutable
 // *=========================================================
 
+const s4 = "primitive \n veri \n tiplerinin \n property veya \n metodu olmaz"
+console.log(s4);
+console.log(s4.charAt(5));//t
+console.log(s4.charAt(11));//" "
+console.log(s4.charAt());//" "
+console.log(s4.charAt(s4.length - 1));//z
+
+// *=========================================================
+// *  includes() -case sensitive (büyük küçük harfe duyarlı)
+// *=========================================================
 const cümle = "to be or not to be, that is The questions."
 
-console.log(cümle.includes("to be"));
-console.log(cümle.includes("TO BE"));
+console.log(cümle.includes("TO BE"));//false
+console.log(cümle.includes("to be"));//true
+console.log(cümle.includes("quest"));//true
+console.log(cümle.includes(" "));//true
+console.log(cümle.includes("to be", 14));//false
+console.log(cümle.toUpperCase().includes("TO BE"));//true
 
 //! NOT: incase sentive kullanmak icin kelime baslangicta kucuk veya
 //! buyuk harfe cevirilerek arama yapilabilir.
 
+// *=========================================================
+// *  indexOf() -lastIndexOf() -case sensitive
+// *=========================================================
+const cümle1 = "to be or not to be, that is The questions.";
 
+console.log(cümle1.indexOf("be"));//3
+console.log(cümle1.lastIndexOf("be"));//16
+console.log(cümle1.indexOf("this"));//-1
+console.log(cümle1.indexOf("TO BE"));//-1
+//bulamazsa -1 cevabını döndürür
+console.log(cümle1.indexOf("be", 4));//16
 
+console.log(cümle1.indexOf("be", cümle1.indexOf("be") + 1));
 
 
 // *=========================================================
-// *          indexOf()
+// *  search()
 // *=========================================================
-
-const cümle1 = "to be or not to be, that is The questions."
-console.log(cümle1.indexOf("be")); //3
-console.log(cümle1.lastIndexOf("be")); //16
-console.log(cümle1.indexOf("this"));  //-1 yoktur
-
-
-
-
-
-// *=========================================================
-// *          search()
-// *=========================================================
-
 const cümle2 = "to be or not 9 to be, that is The questions.";
 
 console.log(cümle2.search(/[A-Z]/));//28
@@ -98,32 +105,87 @@ console.log(cümle2.search(/[a-z]/));//0
 console.log(cümle2.search(/[0-9]/));//13
 console.log(cümle2.search(/[.]/));//43
 console.log(cümle2.indexOf("."));//43
-console.log(cümle2.search(/[^a-z]/));//2 boslugu döndürdü
+console.log(cümle2.search(/[^a-z]/));//2=boşluğu döndürdü
 console.log(cümle2.search(/[0-9A-Za-z]/));//0
 // regex=regular expression /[]/
 
 
-
-
 // *=========================================================
-// *          startsWith(), endWith()  ---case sensitive
+// *  startsWith(),endsWith()--case sensitive
 // *=========================================================
+const sarki = "Salına salına sinsice olurum sana!"
 
-const sarki = "Salina salina sinsice olurum sana!"
-
-console.log(sarki.startsWith("sa")); //false
-console.log(sarki.startsWith("Sa")); //True 
-console.log(sarki.startsWith("salina", 7)); //True
-console.log(sarki.endWith("salina", 7)); //True
-
-
-
+console.log(sarki.startsWith("sa"));//false
+console.log(sarki.startsWith("Sa"));//true
+console.log(sarki.startsWith("salına", 7));//true
+console.log(sarki.endsWith("!"));//true
+console.log(sarki.endsWith("salına", 13));//true (13 bitiş old için dahil değil)
 
 //* ----------------------------------------------------------
 //*  slice(beginIndex, endIndex)
 //*  substring(beginIndex, endIndex)
 //* ----------------------------------------------------------
+const veysel = "UZUN İNCE BİR YOLDAYIM GİDİYORUM GÜNDÜZ GECE"
 
-const veysel = "UZUN INCE BIR YOLDAYIM GIDIYORUM GÜNDÜZ GECE"
-console.log(veysel.slice(10));
-console.log(veysel.slice(10, 21));//UZUN
+console.log(veysel.slice(10));//BİR YOLDAYIM GİDİYORUM GÜNDÜZ GECE
+console.log(veysel.slice(17, 21));//DAYI
+console.log(veysel.substring(17, 21));//DAYI
+console.log(veysel.slice(-10));//- ile index belirlerken sondan sayılır ve saymaya -1 den başlanır,- devam edilir
+console.log(veysel.slice(-11, -6));//GÜNDÜ
+console.log(veysel.slice(16, -6));//LDAYIM GİDİYORUM GÜNDÜ
+
+//!substring metodunda - index kullanılmaz
+console.log(veysel.substring(-10));
+console.log(veysel.substring(veysel.length - 3));//son 3 karakteri döndürür
+
+// *=========================================================
+// *  replace(searchFor,replaceWith)-immutable
+// *=========================================================
+
+let oku = "oku Johny gibi, saf olma, saf olma, saf olma"
+console.log(oku);
+
+oku = oku.replace("saf olma", "basarılı ol")
+console.log(oku);//oku Johny gibi, basarılı ol, saf olma, saf olma
+
+console.log(oku.replace(/SAF/i, "fakir"));//oku Johny gibi, basarılı ol, fakir olma, saf olma
+
+console.log(oku.replace(/Saf olma/gi, "zengin ol"));
+//i eklemesi incase sensitive(büyük küçük harfe duyarsız)
+//gi eklemesi de kastettiğim kelimeden ne kadar varsa hepsini, büyük küçük harfe duyarsız bir şekilde değiştir
+
+//**replaceAll() --immutable */
+
+console.log(oku);//oku Johny gibi, basarılı ol, saf olma, saf olma
+console.log(oku.replaceAll("saf olma", "akıllı ol"));
+
+
+//* ----------------------------------------------------------
+//* split= string i diziye çevirir
+//* split(seperator , limit ) =>ikisi de optional
+// //* split("x")=>x lerden ayırır, virgül+boşluk yapar ve yeni dizi döndürür.orjinal diziyi değiştirmez
+//* ----------------------------------------------------------
+
+const tarkan = "Gel gündüzle gece olalım"
+
+console.log(tarkan.split()); //tek elemanlı bir diziye çevirir
+
+console.log(tarkan.split("e"));// e harflerinden ayırarak bir dizi oluşturur
+console.log(tarkan.split(" "));//boşluklardan ayırarark her kelimeyi, dizinin bir elemanı yaptı
+
+console.log(tarkan.split(""));//harflerden ayırarak bir dizi oluşturur
+console.log(tarkan.split("e", 4));//e harflerinden ayırarak oluşan dizinin ilk 4 elemanını al
+
+console.log(tarkan);
+
+
+// *=========================================================
+// *  trim() String in başından ve sonundan boşlukları siler
+// *=========================================================
+
+const ramazan = "    Hos geldin ya Sehr i Ramazan    "
+console.log(ramazan);
+console.log(ramazan.length);//36
+
+console.log(ramazan.trim());
+console.log(ramazan.trim().length);//28
--- class-notes/DE-06-Arrays/arrays.js
+++ class-notes/DE-06-Arrays/arrays.js
@@ -5,6 +5,71 @@
 //* Dizi Tanımlama
 //* ---------------------------------------------------------
 
+//!  1. Yöntem (Array Literal)-tercih edilen yöntem
 
+const yaslar = [30, 54, 22, 18, 5]
+console.log(yaslar.length);
+console.log(yaslar);
 
 //!hata const keyword ü ile tanımlanmış bir diziyi tamamıyla değiştiremezsiniz, ancak tek tek değişikliğe izin verir
+// yaslar=[12,24,5,6] 
+yaslar[0] = 123;
+console.log(yaslar);
+
+//! 2.yöntem (Array Constructor)
+
+const cars = new Array("BMW", "Mercedes", "AUDI")
+console.log(cars);
+
+console.log("******************************");
+const dogum = 1979
+
+const isimler = [
+    "Sefa",
+    "Olcay",
+    "Adem",
+    "Hakan",
+    2023 - dogum,
+    "İpek",
+    true,
+    yaslar
+
+]
+
+//**dizinin elemanlarına veri yazma (indisleme) */
+isimler[6] = false
+isimler[8] = "merhaba"
+
+console.log(isimler);
+console.log(isimler[1]);//Olcay
+console.log(isimler[7]);//[123, 54, 22, 18, 5]
+console.log(isimler[7][2]);//22
+
+console.log(isimler[isimler.length - 2][yaslar.length - 3]); //22
+
+isimler[7][2]--
+console.log(isimler[7][2]);//kalıcı değişiklik
+
+// ?=========================================================
+// ?                DİZİYİ DEĞİŞTİREN METODLAR (MUTATOR)
+// ?=========================================================
+
+//*pop() dizinin son elemanını siler, yazdırırsakta sildiği elemana döndürür
+const meyveler = ["Elma", "Armut", "Muz", "Kivi", "Erik"]
+
+// meyveler.pop()
+console.log("sildiğim meyve:", meyveler.pop(), "dizinin yeni hali:", meyveler);
+
+//*shift() dizinin ilk elemanını siler, yazdırırsakta sildiği elemana döndürür
+console.log(meyveler.shift(), meyveler);
+
+//********************************** */
+
+//*push() dizinin sonuna eleman ekler, yazdırırsak dizinin güncel eleman sayısını döndürür
+
+console.log(meyveler.push("Çilek", "Kavun"), meyveler);
+
+//*unshift() dizinin başına eleman ekler, yazdırırsak dizinin güncel eleman sayısını döndürür
+
+console.log(meyveler.unshift("Ayva"), meyveler);
+console.log(meyveler);
