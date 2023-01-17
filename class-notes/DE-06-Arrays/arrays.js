8ffbd18334ad6c809aa107c66b14c711ae7934c9
olcay-elt <olcay.elt@gmail.com>
2023-01-17 10:34:21 +0100

    Add new file

--- class-notes/DE-06-Arrays/arrays.js
+++ class-notes/DE-06-Arrays/arrays.js
@@ -73,3 +73,96 @@ console.log(meyveler.push("Çilek", "Kavun"), meyveler);
 
 console.log(meyveler.unshift("Ayva"), meyveler);
 console.log(meyveler);
+
+//*reverse() dizinin tamamini ters cevirir
+
+meyveler.reverse()
+
+console.log(meyveler);
+
+//dizinin 5. elemenini ters cevirin
+console.log(meyveler[5].split("").reverse());
+console.log(meyveler);
+
+
+
+//*sort(), string ifadelerde alfabetik sıralar(ascıı değerlerine göre)
+//* sort metodu diziyi iterasyona uğratır ve parametre olarak aldığı arrow fonksiyonunu (a-b>0 önce b yi yaz gibi) dizinin her bir elemanına uygular. Bu sayade küçük sayılar ile büyük sayıları yer değişirerek sıralama işlemini gerçleştirir.
+
+
+const number = [3, 5, 1, 35, 10, 22, 25, 77, 231]
+
+number.sort()
+console.log(number);
+
+//*sadece sort dersek 22 den sonra 231 i sonra 3 ü getirir. bu yüzden alttaki gibi yazarız= bütün sayıları karşılaştır, küçük olanı önce yazdır
+number.sort((a, b) => a - b)
+console.log(number);
+
+meyveler.sort()
+console.log(meyveler);
+// harfleri ascii ye göre siralar kücük harfler sonra gelir
+
+
+//*splice() 1. parametre dizinin eleman ekleyeceğimiz index ini belirtir
+//*2. parametre=0 ise belirttiğim index teki elemanı sağa ittir, artık orada yeni yazdığım olsun
+//*2.parametre=1 ise belirttiğim index teki elemanı sil üstüne yaz
+
+//? ["Armut", "Ayva", "Kavun", "Kivi", "Muz", "karpuz", "Cilek"]
+
+meyveler.splice(2, 0, "Mango")
+console.log(meyveler);
+
+meyveler.splice(3, 1, "Ananas")
+//meyveler[3]="Ananas"
+console.log(meyveler);
+
+
+
+
+// ?=========================================================
+// ?                DİZİ ERISIM  METODLARi (MUTATOR)
+
+//? (diziyi degistirmezler)
+// ?=========================================================
+
+const sayilar1 = [3, 5, 2, "2", "uc", 2, "bes", 5]
+
+//*includes()
+//*************** */
+
+console.log(sayilar1.includes(5)); //true
+console.log(sayilar1.includes("5"));//false
+
+
+//*indexOf(), lastIndexOf("")
+//*************** */
+
+console.log(sayilar1.indexOf("2"));// 3
+console.log(sayilar1.indexOf(5, 2)); // 7
+// yukarda 2 den sonraki 5 in indexini ver 
+
+//* kullanicidan sayi isteyin girilen sayinin hem string hem number hali sayilar dizisinde var mi arastir varsa index ini dondür
+
+
+const sayilar2 = [3, 5, 2, "2", "uc", 2, "bes", 5]
+
+const sayiString = prompt("lütfen bir sayi giriniz")
+
+const sayiNumber = Number(sayiString)
+
+if (sayilar2.includes(sayiString)) {
+    console.log(sayilar2.indexOf(sayiString));
+}
+if (sayilar2.includes(sayiNumber)) {
+    console.log(sayilar2.indexOf(sayiNumber));
+}
+
+//* yukardaki if lerin ikisininde false olmasi durumunda alttaki kod yazilabilir
+
+if (sayilar2.includes(sayiString) == false && sayilar2.includes(sayiNumber) == false) {
+    console.log("aradiginiz sayi dizi de yok");
+
+}
+//*yukarıdaki if lerin ikisinin de false olması durumunda alttaki kod yazılabilir
+if (sayilar2.includes(sayiString) == false && sayilar2.includes(sayiNumber) == false) { console.log("aradığınız sayı dizide yok"); }
