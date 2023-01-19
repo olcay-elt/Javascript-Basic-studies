66924cf2768a3bd73ecf2597cefe3873ea07f474
olcay-elt <olcay.elt@gmail.com>
2023-01-19 14:11:28 +0100

    Add new file

--- class-notes/DE-08-Objects/object.js
+++ class-notes/DE-08-Objects/object.js
@@ -12,22 +12,18 @@
 //* 1-   Object (new) örneği oluşturalım
 //* ---------------------------------------------------------
 
+const cars = new Object({ name: "BMW", engine: "1.6", model: 2000 });
 
-const cars = new Object({
-    name: "BMW",
-    engine: "1,6",
-    model: 2000
-})
 console.log(cars);
+
 console.log(cars.name, cars.engine);
-console.log(cars["model"]);
-//? square bracket ile erisim
+console.log(cars["name"]); //? square bracket ile erisim
 
 //? Square bracket yonteminin en buyuk avantaji key degerini
 //? degisken olarak kullanabilmemizdir.
 
 //* ---------------------------------------------------------
-//* 2-   Object constructor kullanim (OOP ile ayrintili görecegiz)
+//* 2-   Object constructor kullanalım (OOP ile ayrıntılı göreceğiz)
 //* ---------------------------------------------------------
 
 function Calisanlar(id, name, salary) {
@@ -46,9 +42,8 @@ console.log(kisi3);
 //! this keyword'u icinde bulundugu scope'daki objeyi refere eder
 console.log(this); //? Global scope da oldugumuzdan dolayi browser penceresinin en temel objesini yani window'u gosterir
 
-
 //* ---------------------------------------------------------
-//* 3-   Object Literal (en cok kullanilan yol)
+//* 3-   Object Literal (en çok kullanılan yol)
 //* ---------------------------------------------------------
 
 const person = {
@@ -58,7 +53,6 @@ const person = {
     job: "actor",
     languages: ["C", "C++", "PHYTON", "JAVA"],
 };
-
 console.log(person);
 console.log(person.name);
 console.log(person["age"]);
@@ -69,16 +63,15 @@ console.log(person.languages[2]);
 person.age += 5;
 console.log(person.age);
 
-console.log(person.languages.map((a) => a.toLowerCase()));
+console.log(person.languages.map((a) => a.toLowerCase()))
 
 //? yeni bir property ve value eklenebilir
 
-person.job = "director";
+person.location = "America"
 console.log(person);
 
-
 //* ---------------------------------------------------------
-//*    Objects teki yöntemler
+//*           Objects teki yöntemler
 //* ---------------------------------------------------------
 
 const mensch = {
@@ -87,94 +80,93 @@ const mensch = {
     birth: 1970,
     job: "actor",
     hasDriverLicense: true,
+
     yasHesapla: function () {
+
         return new Date().getFullYear() - this.birth
     },
 
     özet: function () {
-        return `${this.name} ${this.yasHesapla()} yasindadir`
+
+        return `${this.name} ${this.yasHesapla()} yasındadır`
     }
 
 };
+
 console.log(mensch);
 console.log(mensch.yasHesapla());
-console.log(mensch.özet())
-
+console.log(mensch.özet());
 //? arrow func'lar aslinda this keyword'lerini kaldirmak icin gelistirilmis bir fonksiyon yazma yontemidir. Bu sebeple, dogrudan global scope'a baglidir.
 //!NOTE: arrow functions ın sözcüksel bağlamı vardır. Yani this keyword ü bir arrow function içinde kullanırsak beklenmedik değerler alabiliriz. arrow kullanınca,  object in içindeki this keyword ü, global scope u (window) ifade ettiği için, this value nun global scope a bağlanmasını önlemek için, object yöntemi içinde normal function u kullanın.
-
 
 // * ======================================================
 // *                  OBJECT ITERATION
 // * ======================================================
 // key lerin belirli bir sırası (index) olmadığı için, diziye alırsak object araması daha tutarlı bir arama performansına sahip olacaktır. Ayrıca diziler arasında döngü yapmak, keys arasında döngü yapmaktan daha hızlıdır, bu nedenle tüm öğeler üzerinde işlem yapmayı planlıyorsanız, bunları bir diziye koymak akıllıca olabilir.
 
-
 const people = [
     {
         name: "Hakan",
         surname: "Kara",
         job: "developer",
-        age: 30
+        age: 30,
     },
-
     {
         name: "Sefa",
-        surname: "Dogan",
+        surname: "Doganay",
         job: "devops",
-        age: 25
+        age: 25,
     },
-
     {
         name: "Adem",
         surname: "Havva",
         job: "data-science",
-        age: 35
+        age: 35,
     },
-
     {
         name: "Olcay",
         surname: "Halk",
-        job: "tester",
-        age: 28
+        job: "developer",
+        age: 28,
     },
-
     {
         name: "Ömer",
         surname: "Bilir",
         job: "team lead",
-        age: 34
+        age: 34,
     },
-]
-console.log(people);
+];
+
 console.log(people[4].name);
+console.log(people);
+// console.log(Object.keys(people));
+// console.log(Object.values(people));
 
+//* Ornek1  people dizisindeki kişilerin name ve job larını göster
+people.forEach((p) => console.log(p["name"], p.job))
 
-//* Example 1 : people dizisindeki kisilerin name ve joblarini göster
+//* Ornek2 yasları 1 er arttır sonucu yeni bir diziye aktar
 
-people.forEach((p) => console.log(p.name, p.job))
-
-//*Example 2 : yaslari 1 er arttir sonucu yeni bir diziye aktar
 const yas = people.map((a) => a.age + 1)
 console.log(yas);
 
-//* yaslari 1 er arttir sonucu dizide kalici dgistir
+//? yasları 1 er arttır sonucu dizide kalıcı değiştir
 people.map((süslü, index, dizi) => (dizi[index].age = süslü.age + 1));
 
-//*Example 3 : yasi 33 ün üstünde olan kisilerin namelerini listele
+//*Ornek3: yası 33 ün üstünde olan kişilerin name lerini listele
 
 people.filter((süslü) => süslü.age > 33).forEach((a) => console.log(a.name))
-
-//*Example 4 : diziye eleman ekle
+//* Ornek4 diziye eleman ekleyelim
 people.push({
     name: "Osman",
-    surname: "Kara",
-    job: "developer",
-    age: 30
+    surname: "Türk",
+    age: 45,
+
 })
+
 console.log(people);
 
-//*Example 5 : her elemanin name ini büyük harf yap ve yaslarini 1 arttirarak yeni bir object olustur
+//* Ornek5 her elemanın name ini büyük harfli yap ve yaslarını 1 arttırarak yeni bir object oluştur
 
 const yeni = people.map((kisi) => ({
     name1: kisi.name.toUpperCase(),
@@ -184,3 +176,67 @@ const yeni = people.map((kisi) => ({
 }))
 console.log(yeni);
 console.log(people);
+
+//* Ornek6 developer olanların adlarını ve yaslarını yeni object olarak saklayınız
+
+const newObject = people.filter((p) => p.job == "developer").map((a) => ({
+    newName: a.name,
+    newAge: a.age,
+
+}))
+
+console.log(newObject);
+console.log(people);
+
+//*Ornek7 dizideki kişilerin yaslarının ortalamasını alınız
+
+const averaj = people.reduce((toplam, kisi) => toplam + kisi.age, 0) / people.length
+
+console.log(averaj);
+
+
+//? nested ********************
+
+const insanlar = {
+    CanKişisi: {
+        name: "Can",
+        surname: "Canan",
+        dob: "1990",
+        job: "developer",
+        salary: "140000",
+        drivingLicense: true,
+    },
+    JohnKişisi: {
+        name: "John",
+        surname: "Sweet",
+        dob: "1990",
+        job: "tester",
+        salary: "110000",
+        drivingLicense: false,
+    },
+    SteveKişisi: {
+        name: "Steve",
+        surname: "Job",
+        dob: "2000",
+        job: "developer",
+        salary: "90000",
+        drivingLicense: true,
+    },
+};
+//? insanlar objesindeki tum salary 'leri yazdirin (Dongu kullanilmali)
+
+//! FOR - IN kullanılabilir, index sonuçta. Ama for of kullanılamaz , verileri almak için itere eden for of object te çalışmaz
+//* for (let i in dizi) {
+//*   // yürütülecek kod bloğu
+//* }
+
+
+for (let i in insanlar) {
+
+    // console.log(i);
+    console.log(insanlar[i].salary);
+}
+
+// insanlar["JohnKişisi"].salary
+// insanlar["CanKişisi"].salary
+// insanlar["SteveKişisi"].salary
