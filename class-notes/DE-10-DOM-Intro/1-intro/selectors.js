52409425e37a8df231b899e08e706d9ae5750955
olcay-elt <olcay.elt@gmail.com>
2023-01-20 12:16:12 +0100

    Add new file

--- class-notes/DE-10-DOM-Intro/1-intro/selectors.js
+++ class-notes/DE-10-DOM-Intro/1-intro/selectors.js
@@ -8,5 +8,23 @@ const buton = document.getElementById("btn")
 
 buton.style.width = "200px"
 buton.style.height = "80px"
-buton.style.backgroundColor = "red"
-buton.style.color = "yellow"
+buton.style.backgroundColor = "green"
+buton.style.color = "white"
+buton.style.fontSize = "40px"
+buton.textContent = "ARA"
+
+//? element in sadece icerigini degistirmek istersek textContent yeterlidir
+
+//* example2 (paragraf style)
+const paragraf = document.getElementById("par1")
+paragraf.style.backgroundColor = "gray"
+paragraf.style.color = "orange"
+paragraf.style.fontSize = "50px"
+paragraf.style.padding = "5px"
+
+
+//* ======================================================
+//*                   GETELEMENTSBYTAGNAME()
+//* tag ismiyle çağırıyoruz.aynı tag den çok olacağı için index le hangisini seçtiğimizi belirtiyoruz (HTMLCollection- Array özellikleri gösterir)
+//*================================================
+//*example 3 (img style)
