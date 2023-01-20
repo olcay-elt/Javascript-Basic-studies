994a5eae6d2b9c84707cdcc8cfb9321377b11eca
olcay-elt <olcay.elt@gmail.com>
2023-01-20 12:04:43 +0100

    Add new files

---
+++ class-notes/DE-10-DOM-Intro/1-intro/selectors.js
@@ -0,0 +1,12 @@
+//* ======================================================
+//*                   GETELEMENTBYID()
+//* ======================================================
+
+//* example1 (Buton style)
+
+const buton = document.getElementById("btn")
+
+buton.style.width = "200px"
+buton.style.height = "80px"
+buton.style.backgroundColor = "red"
+buton.style.color = "yellow"
