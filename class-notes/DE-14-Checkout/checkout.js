eb3aaabe00d9d593bcd2a8ca9d9e1f9d6dab556b
olcay-elt <olcay.elt@gmail.com>
2023-01-30 22:08:21 +0100

    add

--- class-notes/DE-14-Checkout/checkout.js
+++ class-notes/DE-14-Checkout/checkout.js
@@ -4,35 +4,36 @@
 //!table da kullanılacak değişkenler
 const kargo = 15.0;
 const vergi = 0.18;
+let urunler = document.querySelector("#urun-rowlari")
 
 let sepettekiler = [
-  { name: "Vintage Backpack", price: 34.99, adet: 1, img: "./img/photo1.png" },
-  { name: "Levi Shoes", price: 40.99, adet: 1, img: "./img/photo2.png" },
-  { name: "Antique Clock", price: 69.99, adet: 1, img: "./img/photo3.jpg" },
+    { name: "Vintage Backpack", price: 34.99, adet: 1, img: "./img/photo1.png" },
+    { name: "Levi Shoes", price: 40.99, adet: 1, img: "./img/photo2.png" },
+    { name: "Antique Clock", price: 69.99, adet: 1, img: "./img/photo3.jpg" },
 ];
 //!EKRANA BASTIRMA
 sepettekiler.forEach((ürün) => {
- 
+    urunler.innerHTML +=
 
- `<div class="card mb-3" style="max-width: 540px;">
+        `<div class="card mb-3" style="max-width: 540px;">
 
   <div class="row g-0">
 
     <div class="col-md-5">
-      <img src= ${}  class="img-fluid rounded-start" alt="...">
+      <img src= ${ürün.img}  class="img-fluid rounded-start" alt="...">
     </div>
 
     <div class="col-md-7">
 
       <div class="card-body">
       
-        <h5 class="card-title">${}</h5>
+        <h5 class="card-title">${ürün.name}</h5>
         
              <div class="ürün-price">
                     <p class="text-warning h2">$
-                      <span class="indirim-price">${( * 0.7).toFixed(
-                        2
-                      )}</span>
+                      <span class="indirim-price">${(ürün.price * 0.7).toFixed(
+            2
+        )}</span>
                       <span class="h5 text-dark text-decoration-line-through">${} </span>
                     </p>
                   </div>
@@ -45,7 +46,7 @@ sepettekiler.forEach((ürün) => {
                       <button class="btn btn-secondary btn-sm minus">
                         <i class="fas fa-minus"></i>
                       </button>
-                      <p class="d-inline mx-4" id="ürün-adet">${}</p>
+                      <p class="d-inline mx-4" id="ürün-adet">${ürün.adet}</p>
                       <button class="btn btn-secondary btn-sm plus">
                         <i class="fas fa-plus"></i>
                       </button>
@@ -60,7 +61,7 @@ sepettekiler.forEach((ürün) => {
                   </div>
 
                   <div class="mt-2">
-                    Ürün Toplam: $<span class="ürün-toplam">${(* 0.7 *).toFixed(2)}</span>
+                    Ürün Toplam: $<span class="ürün-toplam">${(ürün.price * 0.7 * ürün.adet).toFixed(2)}</span>
                   </div>
       </div>
     </div>
@@ -93,15 +94,15 @@ document.querySelector("#odeme-table").innerHTML = `<table class="table">
 
 //!SİLME
 document.querySelectorAll(".remove-ürün").forEach(() => {
- //!ekrandan sildik
+    //!ekrandan sildik
 
-  //!diziden sildik
+    //!diziden sildik
 
 });
 
 
 
-  
+
 
 
 //!ADET DEĞİŞTİRME
@@ -110,51 +111,51 @@ document.querySelectorAll(".remove-ürün").forEach(() => {
 
 //!burada (-) icon u  ve adet ve (+) icon u ile işim olduğu için, mesela - ye basınca adet (kardeşi) değişsin istediğim için, minus a ulaşıp ona tıklanınca closest ile parent ına oradan da kardeşine ulaş eksilt diyebiliriz. ya da gerekli elementlerin parent ına ulaşıp çocuklarını çağırarak yapabiliriz
 document.querySelectorAll(".minus").forEach((minus) => {
-  
-
-  minus.onclick = () => {
-    //!minus adet değişimini ekrana bastır
-  
-
-    //!sepettekiler de adet değişimini yapalım 
-
-    sepettekiler.map((ürün) => {
-   
-   
-    //!ürün toplam ı ekrana bastırma (her ürün card ında var)
-   
 
 
-  
-
-  //! plus a basınca minus a benzer işlemler
-  document.querySelectorAll(".plus").forEach((plus) => {
-      //!minus adet değişimini ekrana bastır
-
-      //!dizideki adet i  güncelle
-      
-      //!ürün toplam ekrana bastırması. her üründe olan toplam kısmı
-    
-   
-  
-});
- })
+    minus.onclick = () => {
+        //!minus adet değişimini ekrana bastır
 
 
-//******buradan altı table kısmı */
-//! Card toplam değerlerini hesaplayın ve güncelleyin
+        //!sepettekiler de adet değişimini yapalım 
 
-  //! her bir card daki ürün toplam kısımları, buradaki fiyatları toplayıp ara toplam bulmalıyız (table a bastırmak için)
-  const ürünToplam = document.querySelectorAll(".ürün-toplam");
-  //!  Bir NodeListnesne, bir belgeden çıkarılan düğümlerin bir listesidir
+        sepettekiler.map((ürün) => {
 
-  //? araToplam= en alttaki tüm ürünler için vergi kargo hariç sepettekiler fiyatı
 
-  //*önce hesapla sonra altta browser a (DOM) bastır
- 
-  const vergiPrice = araToplam * vergi;
-  const shipping = araToplam > 0 ? kargo : 0;
-  const cardTotal = araToplam + shipping + vergiPrice;
+            //!ürün toplam ı ekrana bastırma (her ürün card ında var)
+
+
+
+
+
+            //! plus a basınca minus a benzer işlemler
+            document.querySelectorAll(".plus").forEach((plus) => {
+                //!minus adet değişimini ekrana bastır
+
+                //!dizideki adet i  güncelle
+
+                //!ürün toplam ekrana bastırması. her üründe olan toplam kısmı
+
+
+
+            });
+        })
+
+
+        //******buradan altı table kısmı */
+        //! Card toplam değerlerini hesaplayın ve güncelleyin
+
+        //! her bir card daki ürün toplam kısımları, buradaki fiyatları toplayıp ara toplam bulmalıyız (table a bastırmak için)
+        const ürünToplam = document.querySelectorAll(".ürün-toplam");
+        //!  Bir NodeListnesne, bir belgeden çıkarılan düğümlerin bir listesidir
+
+        //? araToplam= en alttaki tüm ürünler için vergi kargo hariç sepettekiler fiyatı
+
+        //*önce hesapla sonra altta browser a (DOM) bastır
+
+        const vergiPrice = araToplam * vergi;
+        const shipping = araToplam > 0 ? kargo : 0;
+        const cardTotal = araToplam + shipping + vergiPrice;
 
   //!ekrana table kısmına bastır
 
