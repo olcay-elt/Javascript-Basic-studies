dd6225595dbc67b01b775313f2125c07f52d8026
olcay-elt <olcay.elt@gmail.com>
2023-02-01 12:21:21 +0100

    add

--- class-notes/DE-15-Async/3-fetchApi.js
+++ class-notes/DE-15-Async/3-fetchApi.js
@@ -1,1 +1,7 @@
-fetch("https://api.github.com/users").then((res) => res.json()).then((veri))
+fetch("https://api.github.com/users").then((res) => res.json()).then((veri) => console.log(veri))
+
+fetch("https://api.github.com/users").then((res) => res.json()).then((data) => ekranaBastir(data))
+
+const ekranaBastir = (data) => {
+
+}
