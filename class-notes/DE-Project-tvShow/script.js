348c575737f0478d055dd01572189adf351ae22f
olcay-elt <olcay.elt@gmail.com>
2023-02-02 23:56:37 +0100

    add

--- class-notes/DE-Project-tvShow/script.js
+++ class-notes/DE-Project-tvShow/script.js
@@ -1,1 +1,29 @@
-fetch("./tv-shows.json")
+
+
+const getData = () => {
+    fetch("./tv-shows.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error("url Error");
+            }
+            return res.json();
+        })
+        .then((data) => screen(data));
+};
+
+getData()
+const screen = (data) => {
+
+    data.forEach((films) => {
+        document.querySelector(".card").innerHTML += `<div class="card" style="width: 18rem;">
+  <img src="" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${films.show.name}</h5>
+    <a href="#" class="btn btn-primary">${View}</a>
+  </div>
+</div>
+        
+   
+        
+        `});
+}
