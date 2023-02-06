c9cdbe8f75d61066d28ddf07cbd95ce703b2ffa9
olcay-elt <olcay.elt@gmail.com>
2023-02-06 19:25:49 +0100

    add

--- class-notes/DE-17-Weather-App/weather.js
+++ class-notes/DE-17-Weather-App/weather.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".top-banner form"); const input = document.querySelector(".top-banner form input");
+//.class1.class2 <==> .class1 .class2(parent to child)
+const msgSpan = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".cities");
 
+//authentication(kimlik doğrulama) vs. authorization(yetkilendirme)
+
+//localStorage vs. sessionStorage
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    getWeatherDataFromApi();
+    form.reset();
+});
+
+const getWeatherDataFromApi = () => {
+    alert("getWeatherDataFromApi");
+}
