343a289a23d13d3c78eb7a96a5f155375736e5f1
olcay-elt <olcay.elt@gmail.com>
2023-02-09 09:36:26 +0100

    Add

--- class-notes/DE-18-OOP/1-Object-Literals.js
+++ class-notes/DE-18-OOP/1-Object-Literals.js
@@ -25,3 +25,8 @@ const book2 = {
     özetFunction: function () {
         return `${this.title} kitabini ${this.yazar} ${this.year} yilinda yazmistir`
     }
+}
+
+//? object literals yöntemi ile fazla sayıda yeni  Object ler oluşturmak oldukça zahmetlidir, ayrıca programcılık yaklaşımı açısından da çok tekrar içerdiği için uygun değildir
+  // DRY (Dont Repeat Yourself) Prensibi için uygun değildir
+//? SOLUTION:  Object Constructor kullan (ES5 and ES6)
