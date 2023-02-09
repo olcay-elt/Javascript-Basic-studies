65a2a002f57b3d932050bfa6b85572578a818d50
olcay-elt <olcay.elt@gmail.com>
2023-02-09 10:45:41 +0100

    Add

---
+++ class-notes/DE-18-OOP/3-classes-ES6.js
@@ -0,0 +1,7 @@
+//* ======================================================
+//*             OOP - Classes (ES6)
+//* ======================================================
+
+//? Classlar,  objects oluşturmak için template (şablondur).
+//? JS deki Classes,   prototypes üzerine kuruludur,  aynı zamanda ES5 syntax ından farklı bazı syntax and semantics e sahiptir .
+//? Aslında  class keyword ü  ES6 da tanıtılmıştır, but is syntactical sugar (yalnızca dili kullanmanın bazı yönlerini daha kolay ve genel olarak daha keyifli hale getirmek için eklenen bir dil özelliği için kullanılan bir terimdir.), JavaScript  prototype-based dir.
