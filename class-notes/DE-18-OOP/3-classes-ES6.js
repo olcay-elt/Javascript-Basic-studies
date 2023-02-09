e95ef39d9a02472bf0fb0295878d7a9f0335d369
olcay-elt <olcay.elt@gmail.com>
2023-02-09 10:55:20 +0100

    Add

--- class-notes/DE-18-OOP/3-classes-ES6.js
+++ class-notes/DE-18-OOP/3-classes-ES6.js
@@ -5,3 +5,26 @@
 //? Classlar,  objects oluşturmak için template (şablondur).
 //? JS deki Classes,   prototypes üzerine kuruludur,  aynı zamanda ES5 syntax ından farklı bazı syntax and semantics e sahiptir .
 //? Aslında  class keyword ü  ES6 da tanıtılmıştır, but is syntactical sugar (yalnızca dili kullanmanın bazı yönlerini daha kolay ve genel olarak daha keyifli hale getirmek için eklenen bir dil özelliği için kullanılan bir terimdir.), JavaScript  prototype-based dir.
+
+
+class Book {
+    constructor(a, b, c) {
+        this.yazar = a;
+        this.title = b;
+
+        this.year = c;
+        this.özetFunction = function () {
+            return ` ${this.title} kitabını ${this.yazar} ${this.year} yılında yazmıştır`;
+        };
+    }
+
+    yilHesapla() {
+        return new Date().getFullYear() - this.year
+    }
+
+}
+
+const book1 = new Book("M.Akif Ersoy", "safahat", 1910)
+
+console.log(book1)
+console.log(book1.yilHesapla);
