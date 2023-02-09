ee3c2ab7a0b63b4df4d8faa1991c243e82b67e6b
olcay-elt <olcay.elt@gmail.com>
2023-02-09 11:39:04 +0100

    Add

---
+++ class-notes/DE-18-OOP/4-Polymorphism.js
@@ -0,0 +1,9 @@
+//* ======================================================
+//*     OOP - Inheritance and Polymorphism(ES6)
+//*         (kalıtım,miras)ve(çok biçimlilik)=============================================
+
+//? Extends keyword ü, parent class ın işlevselliğini child class a genişletmek (extend) için kullanılır..(INHERITANCE)
+
+//* Polymorphism  object-oriented programming concept li bir programlama kavramıdır,bir variable ın, nesnenin, yada fonksiyonun,birden çok form alma özelliğini ifade eder
+
+//* In the OOP, Polymorphism genellikle  Overloading and Overriding le gerçekleştirilir (overriding=aynı ada sahip iki işlev tanımlarsanız, tanımlanan sonuncusu önceden tanımlanmış sürümü geçersiz kılar ve işleve her çağrı yapıldığında, son tanımlanan işlev yürütülür.)(overloading=overloading demektir, javascriptte böyle denmez, aynı adlarla fonksiyonu işlemek denir ve js  direk overloading i desteklemez, manuel uğraşmak gerekir )
