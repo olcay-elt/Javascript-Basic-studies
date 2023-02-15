919c9f6e346416175444b750ee1935549494deab
olcay-elt <olcay.elt@gmail.com>
2023-02-15 21:58:49 +0100

    Add

---
+++ class-notes/DE-19-Module/modules/index.js
@@ -0,0 +1,16 @@
+import ugurla, { topla, CALISMA_SAATI, inc, dec, benimAdim as name } from "./myModule.js"
+
+// import ugurla from "./myModule"
+
+console.log(topla(3, 5));
+
+console.log(CALISMA_SAATI);
+
+console.log("ismim:", name);
+
+console.log(inc(7));
+console.log(inc(7));
+
+console.log(dec(1));
+
+ugurla()
